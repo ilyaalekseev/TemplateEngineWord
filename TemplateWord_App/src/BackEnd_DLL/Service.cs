585834fe804698c6cc48989a98f220d51aab903e
SoshlikKiller <pasha2013rus@gmail.com>
2020-08-29 14:07:45 +0500

    исправил 3 шаблонизатора(дабы меньше кода было и не вручную все так сказать) главное код получился добрый и душевный

--- TemplateWord_App/src/BackEnd_DLL/Service.cs
+++ TemplateWord_App/src/BackEnd_DLL/Service.cs
@@ -32,14 +32,6 @@ namespace BackEnd_DLL
                 CreateTaskTemplate(course, faculty);
         }
 
-        public static FieldContent GetPlan(Dictionary<string, string> _dic, int counter)
-        {
-            string numb_of_plan = "plan_" + counter.ToString();
-            if (_dic.ContainsKey(numb_of_plan))
-                return new FieldContent("part_of_plan", _dic[numb_of_plan] + ".");
-            else
-                return null;
-        }
 
         private void CreateTaskTemplate(string course, string faculty)
         {
@@ -51,57 +43,33 @@ namespace BackEnd_DLL
             {
                 DocX document = DocX.Load(pathDocument + "task.docx");
                 document.SaveAs(pathDocument + "task " + task._dic["name_of_student"] + ".docx");
-                //FieldContent[] lst = new FieldContent[10];
-                //lst[0] = new FieldContent("head_position", task._dic["head_position"]);
-                //lst[1] = new FieldContent("head_position", task._dic["head_position"]);
-                //var valTmp = new Content(lst);
-                var valuesToFill = new Content(
-                    new FieldContent("head_position", task._dic["head_position"]),
-                    new FieldContent("head_name", task._dic["head_name"]),
-                    new FieldContent("Practic_type", task._dic["Practic_type"]),
-                    new FieldContent("Practic_type_2", task._dic["Practic_type_2"]),
-                    new FieldContent("name_of_student", task._dic["name_of_student"]),
-                    new FieldContent("place_of_practic", task._dic["place_of_practic"]),
-                    new FieldContent("date_start_1", task._dic["date_start_1"]),
-                    new FieldContent("date_start_2", task._dic["date_start_2"]),
-                    new FieldContent("date_end_1", task._dic["date_end_1"]),
-                    new FieldContent("date_end_2", task._dic["date_end_2"]),
-                    new FieldContent("footer_name_of_prepod", task._dic["footer_name_of_prepod"]),
-                    new FieldContent("footer_name_of_student", task._dic["footer_name_of_student"])
-                    );
+                FieldContent[] lst = new FieldContent[task._dic.Count];
+                int count_content = 0;
+                foreach (var pair in task._dic)
+                {
+                    if (pair.Key.Substring(0, 4) != "plan")
+                        lst[count_content++] = new FieldContent(pair.Key, pair.Value);
+                }
+                var valuesToFill = new Content(lst);
                 using (var outputDocument = new TemplateProcessor(pathDocument + "task " + task._dic["name_of_student"] + ".docx")
                     .SetRemoveContentControls(true))
                 {
                     outputDocument.FillContent(valuesToFill);
                     outputDocument.SaveChanges();
                 }
-                var valuesToFill2 = new Content(
-                    new ListContent("plan")
-                        .AddItem(GetPlan(task._dic, 1))
-                        .AddItem(GetPlan(task._dic, 2))
-                        .AddItem(GetPlan(task._dic, 3))
-                        .AddItem(GetPlan(task._dic, 4))
-                        .AddItem(GetPlan(task._dic, 5))
-                        .AddItem(GetPlan(task._dic, 6))
-                        .AddItem(GetPlan(task._dic, 7))
-                        .AddItem(GetPlan(task._dic, 8))
-                        .AddItem(GetPlan(task._dic, 9))
-                        .AddItem(GetPlan(task._dic, 10))
-                        .AddItem(GetPlan(task._dic, 11))
-                        .AddItem(GetPlan(task._dic, 12))
-                        .AddItem(GetPlan(task._dic, 13))
-                        .AddItem(GetPlan(task._dic, 14))
-                        );
+                var planing = new ListContent("plan");
+                foreach (var pair in task._dic)
+                {
+                    if (pair.Key.Substring(0, 4) == "plan")
+                        planing.AddItem(new FieldContent("part_of_plan", task._dic[pair.Key] + "."));
+                }
+                var valuesToFill2 = new Content(planing);
                 using (var outputDocument = new TemplateProcessor(pathDocument + "task " + task._dic["name_of_student"] + ".docx")
                     .SetRemoveContentControls(true))
                 {
                     outputDocument.FillContent(valuesToFill2);
                     outputDocument.SaveChanges();
                 }
-                DocX doc = DocX.Load(pathDocument + "task " + task._dic["name_of_student"] + ".docx");
-
-                doc.RemoveParagraphAt(0);
-                doc.Save();
             }
         }
 
@@ -115,20 +83,11 @@ namespace BackEnd_DLL
             {
                 DocX document = DocX.Load(pathDocument + "diary.docx");//создаю копию шаблона в той же директории, что и шаблон
                 document.SaveAs(pathDocument + "diary " + diary._dic["name_of_student_full"] + ".docx");// в том же каталоге создаю заполненный дневник на конкретного слушателя
-                var valuesToFill = new Content(
-                    new FieldContent("Practic_type", diary._dic["Practic_type"]),
-                    new FieldContent("Practic_type_2", diary._dic["Practic_type_2"]),
-                    new FieldContent("name_of_student_full", diary._dic["name_of_student_full"]),
-                    new FieldContent("date_start", diary._dic["date_start"]),
-                    new FieldContent("date_end", diary._dic["date_end"]),
-                    new FieldContent("footer_date", diary._dic["footer_date"]),
-                    new FieldContent("head_prepod", diary._dic["head_prepod"]),
-                    new FieldContent("head_date_1", diary._dic["head_date_1"]),
-                    new FieldContent("rank_of_student", diary._dic["rank_of_student"]),
-                    new FieldContent("footer_name_of_student", diary._dic["footer_name_of_student"]),
-                    new FieldContent("name_of_student_RP", diary._dic["name_of_student_RP"]),
-                    new FieldContent("head_date_2", diary._dic["head_date_2"])
-                    );
+                FieldContent[] lst = new FieldContent[diary._dic.Count];
+                int count_content = 0;
+                foreach (var pair in diary._dic)
+                { lst[count_content++] = new FieldContent(pair.Key, pair.Value); }
+                var valuesToFill = new Content(lst);
                 using (var outputDocument = new TemplateProcessor(pathDocument + "diary " + diary._dic["name_of_student_full"] + ".docx")
                     .SetRemoveContentControls(true))
                 {
@@ -149,33 +108,11 @@ namespace BackEnd_DLL
             {
                 DocX document = DocX.Load(pathDocument + "feedback.docx");
                 document.SaveAs(pathDocument + "feedback " + feed._dic["name_of_student"] + ".docx");
-                var valuesToFill = new Content(
-                    new FieldContent("head_position", feed._dic["head_position"]),
-                    new FieldContent("head_rank", feed._dic["head_rank"]),
-                    new FieldContent("head_name", feed._dic["head_name"]),
-                    new FieldContent("head_date", feed._dic["head_date"]),
-                    new FieldContent("Practical_type", feed._dic["Practical_type"]),
-                    new FieldContent("Practical_type_2", feed._dic["Practical_type_2"]),
-                    new FieldContent("number_of_group", feed._dic["number_of_group"]),
-                    new FieldContent("faculty", feed._dic["faculty"]),
-                    new FieldContent("institute", feed._dic["institute"]),
-                    new FieldContent("rank_of_student", feed._dic["rank_of_student"]),
-                    new FieldContent("name_of_student", feed._dic["name_of_student"]),
-                    new FieldContent("practic_position_of_student", feed._dic["practic_position_of_student"]),
-                    new FieldContent("place_of_practic_full", feed._dic["place_of_practic_full"]),
-                    new FieldContent("date_1_start", feed._dic["date_1_start"]),
-                    new FieldContent("date_2_start", feed._dic["date_2_start"]),
-                    new FieldContent("date_1_end", feed._dic["date_1_end"]),
-                    new FieldContent("date_2_end", feed._dic["date_2_end"]),
-                    new FieldContent("name_of_student_2", feed._dic["name_of_student_2"]),
-                    new FieldContent("skill_of_practic", feed._dic["skill_of_practic"]),
-                    new FieldContent("mark", feed._dic["mark"]),
-                    new FieldContent("footer_position", feed._dic["footer_position"]),
-                    new FieldContent("footer_rank", feed._dic["footer_rank"]),
-                    new FieldContent("footer_name_of_prepod", feed._dic["footer_name_of_prepod"]),
-                    new FieldContent("footer_date", feed._dic["footer_date"])
-
-                );
+                FieldContent[] lst = new FieldContent[feed._dic.Count];
+                int count_content = 0;
+                foreach (var pair in feed._dic)
+                { lst[count_content++] = new FieldContent(pair.Key, pair.Value); }
+                var valuesToFill = new Content(lst);
                 using (var outputDocument = new TemplateProcessor(pathDocument + "feedback " + feed._dic["name_of_student"] + ".docx")
                 .SetRemoveContentControls(true))
                 {
