b3570177f5ea680bc26cda0b87b438ae1fe25ade
nukce <53974486+nukce@users.noreply.github.com>
2020-08-29 15:53:32 +0300

    Добавил функции: RewritingToTable() для перезаписи элементов таблицы, DeleteElementById() для удаления элемента, подправил функции для новой бд из 2-х таблиц

--- TemplateWord_App/src/BackEnd_DLL/ManagingRequestsBD.cs
+++ TemplateWord_App/src/BackEnd_DLL/ManagingRequestsBD.cs
@@ -34,16 +34,16 @@ namespace BackEnd_DLL
 		public bool CreateTable(string tableName, string columnName, string columnType)
 		{
 			if (tableName != String.Empty && columnName != String.Empty && columnType != String.Empty)
-            {
+			{
 				string sql = "CREATE TABLE " + tableName + " (" + columnName + " " + columnType + ");";
-	
+
 				try
-                {
+				{
 					MySqlCommand command = new MySqlCommand(sql, _con);
 					command.ExecuteNonQuery();
 				}
-				catch(Exception e)
-                {
+				catch (Exception e)
+				{
 					Console.WriteLine("{0} Exception caught.", e);
 					return false;
 				}
@@ -56,22 +56,22 @@ namespace BackEnd_DLL
 			Возвращает таблицу tableName в виде двумерного массива строк
 		*/
 		public string[,] GetTable(string tableName)
-        {
+		{
 			if (tableName != String.Empty)
-            {
+			{
 				return GetRowsWithCondition(tableName, ";");
 			}
 			return null;
-        }
+		}
 
 		/*
 			Изменяет старое значение в таблице tableName в колонке columnName
 			на новое значение newValue в строке с номером id
 		*/
-		public bool ChangeColumnValue(string tableName, string columnName,  string newValue, string id)
-        {
+		public bool ChangeColumnValue(string tableName, string columnName, string newValue, string id)
+		{
 			if (tableName != String.Empty && columnName != String.Empty && newValue != String.Empty && id != String.Empty)
-            {
+			{
 				string sql = "UPDATE `template_engine`.`" + tableName + "` SET `" + columnName + "` = '" + newValue + "' WHERE (`id` = '" + id + "');";
 				try
 				{
@@ -106,28 +106,31 @@ namespace BackEnd_DLL
 
 
 				string sql = "INSERT INTO `template_engine`.`" + tableName + "` ";
+				//INSERT INTO `template_engine`.`teachers` (`name`, `rank`, `position`) VALUES ('Хер С Горы', 'жопник', 'шо-то там');
+				//UPDATE `template_engine`.`teachers` SET `rank` = 'звание2', `department` = '101' WHERE (`id` = '1');
+
 
 				//кол-во столбцов в переданном массиве и кол-во столбцов в таблице должно совпадать
 				if (colCount == columnNamesCount)
-                {
+				{
 
 					//собираем строки с названием стоблцов и их значениями и формируем 
 					for (int i = 0; i < rowCount; ++i)
-                    {
+					{
 						string sqlColumns = "(";
 						string sqlValues = "(";
 						for (int j = 0; j < colCount; ++j)
-                        {
-							if (recordsArr[i,j] != String.Empty)
-                            {
+						{
+							if (recordsArr[i, j] != String.Empty)
+							{
 								sqlColumns += "`" + columnNamesArr[j] + "`, ";
 								sqlValues += "'" + recordsArr[i, j] + "', ";
 							}
-                        }
+						}
 
 						//обрезаем последний пробел и запятую
 						sqlColumns = sqlColumns.Remove(sqlColumns.Length - 2, 2);
-						sqlValues =  sqlValues.Remove(sqlValues.Length - 2, 2);
+						sqlValues = sqlValues.Remove(sqlValues.Length - 2, 2);
 						sqlColumns += ") ";
 						sqlValues += ");";
 						string currentSql = sql + sqlColumns + "VALUES " + sqlValues;
@@ -145,29 +148,112 @@ namespace BackEnd_DLL
 							continue;
 						}
 					}
-                }
+				}
 				return success;
 			}
 			return 0;
 		}
 
 		/*
+			Перезаписывает записи со значениями массива recordsArr в таблицу tableName
+			Внимание: массив recordsArr должен содержать кол-во столбцов равное кол-во столбцов в таблице tableName (и тот же порядок)
+		*/
+		public int RewritingToTable(string tableName, string[,] recordsArr)
+		{
+			if (tableName != String.Empty)
+			{
+				//получаем размеры массива на входе и массив названия столбцов из таблицы
+				int success = 0;
+				int rowCount = recordsArr.GetLength(0);
+				int colCount = recordsArr.GetLength(1);
+				string[] columnNamesArr = GetColumnNames(tableName);
+				int columnNamesCount = columnNamesArr.Length;
+
+
+				string sql = "UPDATE `template_engine`.`" + tableName + "` SET ";
+				//INSERT INTO `template_engine`.`teachers` (`name`, `rank`, `position`) VALUES ('Хер С Горы', 'жопник', 'шо-то там');
+				//UPDATE `template_engine`.`teachers` SET `rank` = 'звание2', `department` = '101' WHERE (`id` = '1');
+
+
+				//кол-во столбцов в переданном массиве и кол-во столбцов в таблице должно совпадать
+				if (colCount == columnNamesCount)
+				{
+
+					//собираем строки с названием стоблцов и их значениями и формируем 
+					for (int i = 0; i < rowCount; ++i)
+					{
+						string sqlCurrent = sql;
+						for (int j = 0; j < colCount; ++j)
+						{
+							if (recordsArr[i, j] != String.Empty)
+							{
+								sqlCurrent += "`" + columnNamesArr[j] + "`" + " = '" + recordsArr[i, j] + "', ";
+							}
+						}
+
+						//обрезаем последний пробел и запятую
+						sqlCurrent = sqlCurrent.Remove(sqlCurrent.Length - 2, 2);
+						sqlCurrent += " WHERE (`id` = '" + recordsArr[i, 0] + "');";
+
+						//пробуем отправить запрос
+						try
+						{
+							MySqlCommand command = new MySqlCommand(sqlCurrent, _con);
+							command.ExecuteNonQuery();
+							++success;
+						}
+						catch (Exception e)
+						{
+							Console.WriteLine("{0} Exception caught.", e);
+							continue;
+						}
+					}
+				}
+				return success;
+			}
+			return 0;
+		}
+
+		/*
+			Удаляет элемент с номером id в таблице tableName
+		*/
+		public bool DeleteElementById(string tableName, string id)
+		{
+			if (tableName != String.Empty && id != String.Empty)
+			{
+				string sql = "DELETE FROM `template_engine`.`" + tableName + "` WHERE (`id` = '" + id + "');";
+				try
+				{
+					MySqlCommand command = new MySqlCommand(sql, _con);
+					command.ExecuteNonQuery();
+					return true;
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("{0} Exception caught.", e);
+					return false;
+				}
+			}
+			return false;
+		}
+
+		/*
 			Возвращает массив имен колонок таблицы tableName
 		*/
 		public string[] GetColumnNames(string tableName)
-        {
+		{
 			string sql = "SELECT * FROM " + tableName + ";";
 			try
-            {
+			{
 				MySqlCommand command = new MySqlCommand(sql, _con);
 				MySqlDataReader reader = command.ExecuteReader();
 
 				int rowCount = reader.FieldCount;
 				string[] columnNamesArr = new string[rowCount];
 				for (int i = 0; i < rowCount; ++i)
-                {
+				{
 					columnNamesArr[i] = reader.GetName(i);
-                }
+				}
 				reader.Close();
 				return columnNamesArr;
 			}
@@ -182,13 +268,13 @@ namespace BackEnd_DLL
 			Возвращает лист всех преподавателей 
 		*/
 		public List<Teacher> GetTeachers()
-        {
+		{
 			List<Teacher> teachersList = new List<Teacher>();
 			string[,] teachersArr = GetTable("teachers");
 			int rowCount = teachersArr.GetLength(0);
 
 			for (int i = 0; i < rowCount; ++i)
-            {
+			{
 				//получаем массив с данными о преподе и заполняем структуру
 				string[] teacherArr = GetDesiredRowFromArr(teachersArr, i);
 				Teacher currentTeacher = new Teacher();
@@ -205,7 +291,7 @@ namespace BackEnd_DLL
 			Возвращает список всех студентов
 		*/
 		public List<Student> GetStudents()
-        {
+		{
 			List<Student> studentsList = new List<Student>();
 			string[,] studentsArr = GetTable("students");
 			int rowCount = studentsArr.GetLength(0);
@@ -214,9 +300,9 @@ namespace BackEnd_DLL
 			{
 				//получаем массив с данными об ученике, практике и заполняем структуру
 				string[] studentArr = GetDesiredRowFromArr(studentsArr, i);
-				string[] practiceArr = GetPracticeById(studentArr[0]);
+				//string[] practiceArr = GetPracticeById(studentArr[0]);
 				Student currentStudent = new Student();
-				MakeStudent(studentArr, practiceArr, currentStudent);
+				MakeStudent(studentArr, currentStudent);
 
 				studentsList.Add(currentStudent);
 			}
@@ -227,7 +313,7 @@ namespace BackEnd_DLL
 			Заполняет структуру Teacher данными из массива строк, полученного из бд
 		*/
 		public void MakeTeacher(string[] teacherArr, Teacher teacher)
-        {
+		{
 			int i = 0;
 
 			teacher.id = teacherArr[i++];
@@ -247,11 +333,11 @@ namespace BackEnd_DLL
 		/*
 			Заполняет структуру student значениями из массива studentArr
 		*/
-		public void MakeStudent(string[] studentArr, string[] practiceArr, Student student)
-        {
+		public void MakeStudent(string[] studentArr, Student student)
+		{
 			int i = 0;
 
-			//заполнение инфо из массива studentArr
+			//заполнение инфо из массива studentArr личных данных
 			student.id = studentArr[i++];
 			string[] nameArr = studentArr[i++].Split(' ');
 			student.secondName = nameArr[0];
@@ -260,33 +346,31 @@ namespace BackEnd_DLL
 			student.rank = studentArr[i++];
 			student.faculty = studentArr[i++];
 			student.course = studentArr[i][2].ToString(); // третья цифра - номер курса
-			student.group = studentArr[i];
+			student.group = studentArr[i++];
 
-			i = 1;
-
-			//заполнение инфо из массива practiceArr
-			student.teacherId = practiceArr[i++];
-			student.practiceTypeOne = practiceArr[i++];
-			student.practiceTypeTwo = practiceArr[i++];
-			student.position = practiceArr[i++];
-			student.location = practiceArr[i++];
-			student.date = practiceArr[i++];
-			student.skill = practiceArr[i++];
-			student.mark = practiceArr[i];
+			//заполнение инфо из массива studentArr об практике
+			student.teacherId = studentArr[i++];
+			student.practiceTypeOne = studentArr[i++];
+			student.practiceTypeTwo = studentArr[i++];
+			student.position = studentArr[i++];
+			student.location = studentArr[i++];
+			student.date = studentArr[i++];
+			student.skill = studentArr[i++];
+			student.mark = studentArr[i];
 		}
 
 		/*
 			Возвращает список студентов для преподавателя с teacherId
 		*/
-		public List<Student> GetStudentsForTeacher (string teacherId)
-        {
+		public List<Student> GetStudentsForTeacher(string teacherId)
+		{
 			List<Student> studentsList = new List<Student>();
 			//string sqlStudentsId = "SELECT student_id FROM template_engine.practices WHERE teacher_id = '" + teacherId + "';";
 			//string sqlStudents = "SELECT * FROM template_engine.students WHERE id in ";
 
 			//получаем массив с id учеников для препода
 			string conditionStudentsId = " WHERE teacher_id = '" + teacherId + "';";
-			string[,] studentsIdArrs = GetRowsWithCondition("practices", conditionStudentsId);
+			string[,] studentsIdArrs = GetRowsWithCondition("students", conditionStudentsId);
 			if (studentsIdArrs == null)
 				return studentsList;
 
@@ -294,52 +378,52 @@ namespace BackEnd_DLL
 			string conditionStudents = " WHERE id in (";
 			int idCounts = studentsIdArrs.GetLength(0);
 			for (int i = 0; i < idCounts; ++i)
-            {
+			{
 				conditionStudents += studentsIdArrs[i, 0] + ", ";
-            }
+			}
 			conditionStudents = conditionStudents.Remove(conditionStudents.Length - 2, 2) + ");";
 
 			//получаем инфу об учениках, заполняем структуру, добавляем в список
 			string[,] studentArrs = GetRowsWithCondition("students", conditionStudents);
 			int studentsCount = studentArrs.GetLength(0);
 			for (int i = 0; i < studentsCount; ++i)
-            {
+			{
 				string[] studentArr = GetDesiredRowFromArr(studentArrs, i);
-				string[] practiceArr = GetPracticeById(studentArr[0]);
+				//string[] practiceArr = GetPracticeById(studentArr[0]);
 				Student student = new Student();
-				MakeStudent(studentArr, practiceArr, student);
+				MakeStudent(studentArr, student);
 				studentsList.Add(student);
 			}
 			return studentsList;
-        }
+		}
 
 		/*
 			Возвращает массив с информацией о практике ученика с номером id
 		*/
-		public string[] GetPracticeById(string id)
-        {
-			//string sql = "SELECT * FROM template_engine.practices WHERE student_id = '" + id + "';";
-			string condition = " WHERE student_id = '" + id + "';";
-			string[,] practice = GetRowsWithCondition("practices", condition);
-			if (practice == null)
-            {
-				string[] practiceArrNull = new string[9]; // TODO: кол-во столбцов в таблице вынести в глоб переменую
-				for (int i = 0; i < practiceArrNull.Length; ++i)
-                {
-					practiceArrNull[i] = "";
-                }
-				return practiceArrNull;
-            }
+		/*	public string[] GetPracticeById(string id)
+			{
+				//string sql = "SELECT * FROM template_engine.practices WHERE student_id = '" + id + "';";
+				string condition = " WHERE student_id = '" + id + "';";
+				string[,] practice = GetRowsWithCondition("practices", condition);
+				if (practice == null)
+				{
+					string[] practiceArrNull = new string[9]; // TODO: кол-во столбцов в таблице вынести в глоб переменую
+					for (int i = 0; i < practiceArrNull.Length; ++i)
+					{
+						practiceArrNull[i] = "";
+					}
+					return practiceArrNull;
+				}
 
-			string[] practiceArr = GetDesiredRowFromArr(practice, 0);
-			return practiceArr;
-		}
-
+				string[] practiceArr = GetDesiredRowFromArr(practice, 0);
+				return practiceArr;
+			}
+		*/
 		/*
 		  Возвращает массив с инфой об учителе по его id
 		 */
 		public string[] GetTeacherById(string id)
-        {
+		{
 			//string sql = "SELECT * FROM template_engine.teachers WHERE id = '" + id + "';";
 			string condition = " WHERE id = '" + id + "';";
 			string[,] teacherArrs = GetRowsWithCondition("teachers", condition);
@@ -351,18 +435,18 @@ namespace BackEnd_DLL
 			Возвращает структуру студента по его id
 			Внимание: рабоате или нет хз - не отлаживал
 		*/
-		public Student GetStudentById (string id)
-        {
+		public Student GetStudentById(string id)
+		{
 			Student student = new Student();
 			if (id != String.Empty)
-            {
+			{
 				string sql = "SELECT * FROM template_engine.students WHERE id = '" + id + "';";
 				string condition = " WHERE id = '" + id + "';";
 				string[,] studentArrs = GetRowsWithCondition("students", condition);
 				string[] studentArr = GetDesiredRowFromArr(studentArrs, 0);
 
-				string[] practiceArr = GetPracticeById(studentArr[0]);
-				MakeStudent(studentArr, practiceArr, student);
+				//string[] practiceArr = GetPracticeById(studentArr[0]);
+				MakeStudent(studentArr, student);
 			}
 			return student;
 
@@ -372,8 +456,8 @@ namespace BackEnd_DLL
 			Возвращает массив строк со значениями из таблицы tableName,
 			удовлетворяющие условию condition
 		*/
-		public string[,] GetRowsWithCondition (string tableName, string condition)
-        {
+		public string[,] GetRowsWithCondition(string tableName, string condition)
+		{
 			string sql = "SELECT * FROM template_engine." + tableName + condition;
 			MySqlCommand command = new MySqlCommand(sql, _con); // запрос на все данные
 			MySqlDataReader reader = command.ExecuteReader(); // получае данные
@@ -416,14 +500,14 @@ namespace BackEnd_DLL
 			Обертка для получения в виде одномерного массива элемента rowNumber
 			из двумерного массива arr
 		*/
-		public string[] GetDesiredRowFromArr (string[,] arr, int rowNumber)
-        {
+		public string[] GetDesiredRowFromArr(string[,] arr, int rowNumber)
+		{
 			int colCount = arr.GetLength(1);
 			string[] desiredRow = new string[colCount];
 			for (int i = 0; i < colCount; ++i)
-            {
+			{
 				desiredRow[i] = arr[rowNumber, i];
-            }
+			}
 			return desiredRow;
 		}
 
@@ -431,11 +515,11 @@ namespace BackEnd_DLL
 			Возвращает утверждающего
 		*/
 		public Teacher GetApprover()
-        {
+		{
 			Teacher approver = new Teacher();
 			string[] approverArr = GetTeacherById("0");
 			MakeTeacher(approverArr, approver);
 			return approver;
-        }
+		}
 	}
 }
